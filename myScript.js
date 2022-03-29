38ffa17380aebabf485844d0d7cc498efc7cab53
Jhoan J. Ramírez <102481805+Dojhoanrd@users.noreply.github.com>
2022-03-29 23:41:19 +0200

    Add files via upload

---
+++ myScript.js
@@ -0,0 +1,139 @@
+var tabButtons=document.querySelectorAll(".tabContainer .buttonContainer button");
+var tabPanels=document.querySelectorAll(".tabContainer  .tabPanel");
+
+function showPanel(panelIndex,colorCode) {
+    tabButtons.forEach(function(node){
+        node.style.backgroundColor="peru";
+        node.style.color="white";
+    });
+    tabButtons[panelIndex].style.backgroundColor="saddlebrown";
+    tabButtons[panelIndex].style.color= "white";
+    tabPanels.forEach(function(node){
+        node.style.display="none";
+    });
+    tabPanels[panelIndex].style.display="block";
+    tabPanels[panelIndex].style.backgroundColor = "saddlebrown";
+}
+showPanel(0, "saddlebrown");
+
+
+
+
+function openPage(pageName, elmnt, color) {
+    // Hide all elements with class="tabcontent" by default */
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    // Remove the background color of all tablinks/buttons
+    tablinks = document.getElementsByClassName("tablink");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].style.backgroundColor = "black";
+    }
+
+    // Show the specific tab content
+    document.getElementById(pageName).style.display = "block";
+
+    // Add the specific color to the button used to open the tab content
+    elmnt.style.backgroundColor = red;
+}
+
+// Get the element with id="defaultOpen" and click on it
+document.getElementById("defaultOpen").click();
+
+function operaciones(op) {
+
+    var ops = {
+        sumar: function sumarNumeros(n1, n2) {
+            return (parseInt(n1) + parseInt(n2));
+        },
+
+        restar: function restarNumeros(n1, n2) {
+            return (parseInt(n1) - parseInt(n2));
+        },
+
+        multiplicar: function multiplicarNumeros(n1, n2) {
+            return (parseInt(n1) * parseInt(n2));
+        },
+
+        dividir: function dividirNumeros(n1, n2) {
+            return (parseInt(n1) / parseInt(n2));
+        }
+
+
+    };
+
+    var operacion;
+
+    switch (op) {
+        case 'sumar':
+            var operando1 = document.getElementById("resultado").value;
+            document.getElementById("resultado").value = operando1 + "+";
+            operacion = document.getElementById("resultado").value;
+            document.getElementById("memoria").value = "suma";
+            break;
+        case 'restar':
+            var operando1 = document.getElementById("resultado").value;
+            document.getElementById("resultado").value = operando1 + "-";
+            operacion = document.getElementById("resultado").value;
+            document.getElementById("memoria").value = "resta";
+            break;
+        case 'multiplicar':
+            var operando1 = document.getElementById("resultado").value;
+            document.getElementById("resultado").value = operando1 + "*";
+            operacion = document.getElementById("resultado").value;
+            document.getElementById("memoria").value = "multiplicacion";
+            break;
+        case 'dividir':
+            var operando1 = document.getElementById("resultado").value;
+            document.getElementById("resultado").value = operando1 + "/";
+            operacion = document.getElementById("resultado").value;
+            document.getElementById("memoria").value = "division";
+            break;
+
+        case 'igual':
+
+            operacion = document.getElementById("resultado").value;
+
+            var memoriaop = document.getElementById("memoria").value;
+
+            switch (memoriaop) {
+                case 'suma':
+                    var operandos = operacion.split("+");
+                    var resultado = ops.sumar(operandos[0], operandos[1]);
+                    document.getElementById("resultado").value = resultado;
+                    break;
+                case 'resta':
+                    var operandos = operacion.split("-");
+                    var resultado = ops.restar(operandos[0], operandos[1]);
+                    document.getElementById("resultado").value = resultado;
+                    break;
+                case 'multiplicacion':
+                    var operandos = operacion.split("*");
+                    var resultado = ops.multiplicar(operandos[0], operandos[1]);
+                    document.getElementById("resultado").value = resultado;
+                    break;
+                case 'division':
+                    var operandos = operacion.split("/");
+                    var resultado = ops.dividir(operandos[0], operandos[1]);
+                    document.getElementById("resultado").value = resultado;
+                    break;
+
+            }
+
+
+            break;
+
+
+    }
+
+
+}
+
+
+
+
+
+
